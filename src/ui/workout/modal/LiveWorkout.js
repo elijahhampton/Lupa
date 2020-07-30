59dc4ea2cf84bbf800d540fb43bc9e7a19e26a7e
Elijah Hampton <elijahhampton@Elijahs-MacBook-Pro.local>
2020-07-30 09:32:12 -0500

    Cleanup

--- src/ui/workout/modal/LiveWorkout.js
+++ src/ui/workout/modal/LiveWorkout.js
@@ -5,15 +5,22 @@ import {
     Text,
     StyleSheet,
     Modal,
+    ScrollView,
     Image,
     SafeAreaView,
     Dimensions,
     TouchableHighlight,
+    TouchableOpacity,
 } from 'react-native';
 
 import {
     ActivityIndicator,
     Divider,
+    IconButton,
+    Button,
+    Menu,
+    Surface,
+    Provider,
 } from 'react-native-paper';
 
 import { Icon } from 'react-native-elements'
@@ -29,7 +36,6 @@ import ThinFeatherIcon from "react-native-feather1s";
 import { LOG_ERROR } from '../../../common/Logger';
 import { Constants } from 'react-native-unimodules';
 import { getLupaProgramInformationStructure } from '../../../model/data_structures/programs/program_structures';
-import { ScrollView } from 'react-native-gesture-handler';
 
 const chartConfig = {
     backgroundGradientFrom: "red",
@@ -106,10 +112,14 @@ class LiveWorkout extends React.Component {
             workoutStructure: ['Workout Name', 'Workout Name', 'Workout Name'],
             currentWorkout: {},
             currentWorkoutStructure: [],
-            currentDay: "",
+            workoutDays: [],
+            currentWorkoutDay: "",
+            currentDayIndex: 0,
+            currentWorkoutIndex: 0,
             playVideo: false,
             ready: false,
-            programData: undefined
+            programData: getLupaProgramInformationStructure(),
+            dayMenuVisible: false,
         }
     }
 
@@ -120,21 +130,73 @@ class LiveWorkout extends React.Component {
     setupLiveWorkout = async () => {
         await this.LUPA_CONTROLLER_INSTANCE.getProgramInformationFromUUID(this.props.route.params.uuid).then(data => {
             this.setState({ programData: data})
+            console.log(data)
         })
+
+
+        await this.loadWorkoutDays()
+        await this.loadCurrentDayWorkouts()
+
 
        await this.setState({ ready: true })
 
-       alert(this.state.ready)
-       
+       alert('TRUE')
     }
 
+    loadWorkoutDays = () => {
+      this.setState({ workoutDays: this.state.programData.program_workout_days })
+      this.loadCurrentDayWorkouts(this.state.programData.program_workout_days[0])
+    }
+
+    loadCurrentDayWorkouts = (day) => {
+      switch(day) {
+        case 'Monday':
+          this.setState({ currentWorkoutDay: 'Monday', currentWorkoutStructure: this.state.programData.program_workout_structure.Monday, currentWorkout: this.state.programData.program_workout_structure.Monday[0] })
+          break;
+          case 'Tuesday':
+            this.setState({ currentWorkoutDay: 'Tuesday', currentWorkoutStructure: this.state.programData.program_workout_structure.Tuesday, currentWorkout: this.state.programData.program_workout_structure.Tuesday[0] })
+            break;
+            case 'Wednesday':
+              this.setState({ currentWorkoutDay: 'Wednesday', currentWorkoutStructure: this.state.programData.program_workout_structure.Wednesday, currentWorkout: this.state.programData.program_workout_structure.Wednesday[0] })
+              break;
+              case 'Thursday':
+                this.setState({ currentWorkoutDay: 'Thursday', currentWorkoutStructure: this.state.programData.program_workout_structure.Thursday, currentWorkout: this.state.programData.program_workout_structure.Thursday[0] })
+                break;
+                case 'Friday':
+                  this.setState({ currentWorkoutDay: 'Friday', currentWorkoutStructure: this.state.programData.program_workout_structure.Friday, currentWorkout: this.state.programData.program_workout_structure.Friday[0] })
+                  break;
+                  case 'Saturday':
+                    this.setState({ currentWorkoutDay: 'Saturday', currentWorkoutStructure: this.state.programData.program_workout_structure.Saturday, currentWorkout: this.state.programData.program_workout_structure.Saturday[0] })
+                    break;
+                    case 'Sunday':
+                      this.setState({ currentWorkoutDay: 'Sunday', currentWorkoutStructure: this.state.programData.program_workout_structurea.Sunday, currentWorkout: this.state.programData.program_workout_structure.Sunday[0] })
+                      break;
+                      default:
+      }
+    }
+
+    handleOnChangeWorkout = () => {
+
+    }
+
+    openDayMenu = () => this.setState({ dayMenuVisible: true })
+
+    closeDayMenu = () => this.setState({ dayMenuVisible: false })
+
+
     getWorkoutMedia = () => {
+        if (this.state.ready && this.state.playVideo === false) {
+            return (
+                <Image style={{width: '100%', height: '100%'}} resizeMode="cover" source={{uri: this.state.programData.program_image}} />
+            )
+        }
+
         try {
-            if (this.state.currentWorkout.workout_media.media_type == "VIDEO") {
+            if (false /*this.state.currentWorkout.workout_media.media_type == "VIDEO"*/) {
                 return (
                     <>
                         <Video
-                            source={{ uri: this.state.currentWorkout.workout_media.uri }}
+                            source={{ uri: this.state.programData.program_workout_data.Monday[0].workout_media.uri }}
                             rate={1.0}
                             volume={0}
                             isMuted={true}
@@ -142,8 +204,7 @@ class LiveWorkout extends React.Component {
                             shouldPlay={this.state.playVideo}
                             isLooping={true}
                             style={{
-                                width: "100%",
-                                height: "100%",
+                                flex: 1,
                                 alignItems: 'center',
                                 justifyContent: 'center',
                             }}
@@ -156,7 +217,9 @@ class LiveWorkout extends React.Component {
                 )
             }
         } catch (err) {
+            alert(err)
             return (
+
                 <View style={{ flex: 1, backgroundColor: '#212121' }}>
 
                 </View>
@@ -201,6 +264,10 @@ class LiveWorkout extends React.Component {
         return null
     }
 
+    renderContent = () => {
+
+    }
+
     renderComponentDisplay = () => {
         if (this.state.ready && typeof(this.state.programData) != 'undefined') {
             return (
@@ -211,80 +278,91 @@ class LiveWorkout extends React.Component {
                     <View style={{ flex: 1 }}>
                         <SafeAreaView style={{ backgroundColor: '#212121' }} />
                         {/* Content */}
-                        <View style={{ flex: 1, backgroundColor: '#212121' }}>
+                        <View style={{ flex: 1, backgroundColor: '#212121', justifyContent: 'center', alignItems: 'center' }}>
 
                         </View>
 
+
+
+                        {/* Video slide Overlay */}
+                        <View style={{width: Dimensions.get('window').width, position: 'absolute', bottom: Constants.statusBarHeight + 80}}>
+                          <ScrollView
+                            horizontal={true}
+                            showsHorizontalScrollIndicator={false}
+                            shouldRasterizeIOS={true}
+                            contentContainerStyle={{justifyContent: 'space-between'}}
+                            deaccelerationRate={0}
+                            snapToInterval={Dimensions.get('window').width / 5}
+                            snapToAlignment={'center'}
+                            centerContent
+                            >
+                            {
+                              this.state.currentWorkoutStructure.map((workout, index, arr) => {
+                                return (
+                                  <TouchableOpacity onPress={() => this.handleOnChangeWorkout(workout)}>
+                                  <Surface key={workout.workout_uid} style={{ backgroundColor: '#212121', elevation: 3, width: Dimensions.get("window").width / 5, height: 50, marginHorizontal: 10, marginVertical: 3, borderRadius: 10, alignItems: "center", justifyContent: "center" }}>
+                                  
+                                  </Surface>
+                                  </TouchableOpacity>
+                                )
+                              })
+                            }
+                          </ScrollView>
+                        </View>
+
                         {/* Overlay on first swiper view */}
-                        <View style={{ ...StyleSheet.absoluteFillObject, paddingTop: Constants.statusBarHeight, justifyContent: 'space-between', flex: 1 }}>
+                        <View style={{ ...StyleSheet.absoluteFillObject, paddingTop: Constants.statusBarHeight, paddingVertical: 30, justifyContent: 'space-between', flex: 1 }}>
                             {/* Top overlayed options */}
-                            <View style={{ width: '100%', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', paddingHorizontal: 10 }}>
-                                <View style={{ flexDirection: 'row', backgroundColor: '#212121' }}>
-                                    <View>
-                                        <Text style={{ fontSize: 20, fontFamily: 'Helvetica-Bold', color: 'white', }}>
-                                            <Text>
-                                                {this.state.programData.program_name}
-                                </Text>
-                                            <MaterialIcon name="arrow-drop-down" size={20} />
-                                        </Text>
-
-                                        <Text style={{ color: 'white', fontFamily: 'Helvetica-Light' }}>
-                                            tap to change the program
-                                </Text>
-                                    </View>
-
-                                </View>
-                                <View style={{ backgroundColor: '#212121' }}>
-                                    <Text style={{ alignSelf: 'center', fontSize: 20, fontFamily: 'Helvetica-Bold', color: 'white', }}>
-                                        <Text>
-                                            Current Day
-                                </Text>
-                                        <MaterialIcon name="arrow-drop-down" size={20} />
-                                    </Text>
-
-                                </View>
+                            <View style={{ width: '100%', flexDirection: 'row', alignItems: 'flex-start', justifyContent: 'space-between', paddingHorizontal: 10, paddingTop: 10 }}>
+                            <View>
+                            <Text style={{color: 'white'}}>
+                                0:00 / 4:30
+                            </Text>
+                        </View>
 
                             </View>
                             <View>
-                                <Divider style={{width: Dimensions.get('window').width, backgroundColor: '#FFFFFF', width: '90%', alignSelf: 'center'}} />
-                                <View style={{ width: '100%', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-evenly', backgroundColor: '#212121', marginVertical: 10 }}>
-                                    
-                                    <Icon
-                                        reverse
-                                        name='info'
-                                        type='feather'
-                                        color='rgba(0,0,0,0.2)'
-                                        size={20}
-                                        style={{ elevation: 10 }}
-                                    />
-
-                                    <Icon
-                                        reverse
-                                        name='message-circle'
-                                        type='feather'
-                                        color='rgba(0,0,0,0.2)'
-                                        size={20}
-                                        style={{ elevation: 10 }}
-                                    />
 
 
-                                    <Icon
-                                        reverse
-                                        name='x'
-                                        type='feather'
-                                        color='rgba(0,0,0,0.2)'
-                                        size={20}
-                                        style={{ elevation: 10 }}
-                                    />
+                                <View style={{paddingHorizontal: 10, width: Dimensions.get('window').width, flexDirection: 'row',alignItems: 'center', justifyContent: 'space-between'}}>
+                                <TouchableOpacity onPress={this.openDayMenu}>
 
+                                <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
+                                <Menu
+                                onInd
+                             visible={this.state.dayMenuVisible}
+                             onDismiss={this.closeDayMenu}
+                             anchor={
+                                 <Text  style={{color: 'white', fontWeight: '600', fontSize: 20, fontFamily: 'Avenir-Roman'}}>
+                                   {this.state.currentWorkoutDay}
+                                 </Text>
+                             }>
+                             {
+                               this.state.workoutDays.map((day, index, arr) => {
+                                 return (
+                                   <Menu.Item key={day} onPress={() => this.loadCurrentDayWorkouts(day)} title={day} />
+                                 )
+                               })
+                             }
+                           </Menu>
+
+
+                                <FeatherIcon name="chevron-down" size={20}  color="white"/>
                                 </View>
+                                </TouchableOpacity>
 
-                                <View style={{width: Dimensions.get('window').width}}>
-                                    <ScrollView horizontal showsHorizontalScrollIndicator={false} shouldRasterizeIOS={true} centerContent contentContainerStyle={{ marginVertical: 10, alignItems: 'center', justifyContent: 'center'}} decelerationRate={0} snapToAlignment='center' snapToInterval={Dimensions.get('window').width}>
-                                        {
-                                            this.renderWorkoutNames()
-                                        }
-                                    </ScrollView>
+
+                                    <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
+
+
+                                    <FeatherIcon
+                                        name="more-horizontal"
+                                        size={30}
+                                        color="white"
+                                        style={{ elevation: 10, paddingHorizontal: 20 }}
+                                    />
+
+                                    </View>
                                 </View>
                             </View>
                         </View>
