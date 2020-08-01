eed09b2b84ca4127ad2a05770c690f10a424aaea
Elijah Hampton <elijahhampton@Elijahs-MacBook-Pro.local>
2020-07-31 22:00:34 -0500

    Adds UpgradeAccount view.

---
+++ src/ui/user/trainer/UpgradeAccount.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import {
+    View,
+    Text,
+    StyleSheet
+} from 'react-native';
+
+const UpgradeAccount = () => {
+    return (
+        <View style={styles.container}>
+            
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#FFFFFF'
+    }
+})
+
+export default UpgradeAccount;
