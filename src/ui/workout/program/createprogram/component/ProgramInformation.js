15c39f7492dc5fe159c98cb3046faaee1d08379f
Elijah Hampton <elijahhampton@Elijahs-MacBook-Pro.local>
2020-08-21 13:22:01 -0400

    Adds checkInputs function back to ProgramInformation.

--- src/ui/workout/program/createprogram/component/ProgramInformation.js
+++ src/ui/workout/program/createprogram/component/ProgramInformation.js
@@ -314,7 +314,7 @@ function ProgramInformation(props) {
 
   const getNextView = () => {
     //check program values
-    let retVal = false //checkInputs()
+    let retVal = checkInputs()
     if (retVal) {
       return;
     }
