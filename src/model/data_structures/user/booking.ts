af73ece6b38fe842d1573c04418ad98f9c66ce63
Elijah Hampton <elijahhampton@Elijahs-MacBook-Pro.local>
2020-11-08 11:44:46 -0500

    Completes configuration of backend toa llow for virtual bookings.  Adds session types to booking structure and handles the appropriate ios permission settings.

--- src/model/data_structures/user/booking.ts
+++ src/model/data_structures/user/booking.ts
@@ -10,6 +10,7 @@ let booking : Booking = {
     status: BOOKING_STATUS.BOOKING_UNKNOWN,
     uid: 0,
     note: "",
+    session_type: ''
 }
 
 export function getNewBookingStructure(start_time, end_time, date, date_requested, trainer_uuid, requester_uuid, trainer_note) : Booking {
@@ -19,7 +20,7 @@ export function getNewBookingStructure(start_time, end_time, date, date_requeste
     booking.date_requested = date_requested;
     booking.trainer_uuid = trainer_uuid;
     booking.requester_uuid = requester_uuid;
-    booking.status = BOOKING_STATUS.BOOKING_REQUESTED;
+    booking.status = Number(BOOKING_STATUS.BOOKING_REQUESTED);
     booking.note = trainer_note;
     booking.uid = Math.random().toString();
     return booking;
@@ -32,7 +33,7 @@ export function getBookingStructure() : Booking {
     booking.date_requested = new Date();
     booking.trainer_uuid = 0;
     booking.requester_uuid = 0;
-    booking.status = BOOKING_STATUS.BOOKING_UNKNOWN;
+    booking.status = Number(BOOKING_STATUS.BOOKING_UNKNOWN);
     booking.note = "";
     return booking;
 }
